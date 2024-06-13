979ad3f257bf8a752311503289a0dca0de80167f
nadya-lynn <154042485+nadya-lynn@users.noreply.github.com>
2024-06-12 22:21:08 -0500

    Tests 1,2,3 passing.Tasks 1,2,3 passing

--- queries.sql
+++ queries.sql
@@ -9,4 +9,6 @@ SELECT name FROM employer WHERE location = "St. Louis City";
 
 --Part 3
 
+DROP TABLE job;
+
 --Part 4
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,9 +1,14 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -13,7 +18,17 @@ public class Employer extends AbstractEntity {
     @Size(min = 1, max = 255, message = "Location must be 1 to 255 characters long.")
     private String location;
 
-    public String Employer;
+    public Employer() {
+
+    }
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private final List<Job> jobs = new ArrayList<>();
 
     public String getLocation() {
         return location;
