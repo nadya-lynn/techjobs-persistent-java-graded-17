b81a30d8ebc01fd2d1369369f2de012e49ff07a4
nadya-lynn <154042485+nadya-lynn@users.noreply.github.com>
2024-06-13 20:50:20 -0500

    All 48 tests are passing!

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,6 +1,7 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -17,7 +18,7 @@ public class Skill extends AbstractEntity {
     @Size(min = 1, max = 255, message = "Description must be 1 to 255 characters long.")
     private String description;
 
-    @OneToMany(mappedBy="skills")
+    @ManyToMany(mappedBy="skills")
     private final List<Job> jobs = new ArrayList<>();
     public Skill() {
 
